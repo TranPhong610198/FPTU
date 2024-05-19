5eb8e6a3e1f72bf93a7044f4d02401b2564197fd
Moriarty <tphong610198@gmail.com>
2024-05-19 21:18:49 +0700

    Update BTVN Stack and Queue

---
+++ CSD201/BTVN_StackAndQueue/Q2_Queue/src/q2_queue/MyQueue.java
@@ -0,0 +1,14 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package q2_queue;
+
+/**
+ *
+ * @author tphon
+ */
+public class MyQueue {
+    
+}
--- LAB211/LetterAndCharacterCount/src/letterandcharactercount/Main.java
+++ LAB211/LetterAndCharacterCount/src/letterandcharactercount/Main.java
@@ -4,6 +4,8 @@
  */
 package letterandcharactercount;
 
+import java.util.*;
+
 /**
  *
  * @author tphon
@@ -14,7 +16,34 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+//        // Tạo đối tượng Scanner để đọc đầu vào từ người dùng
+//        Scanner scanner = new Scanner(System.in);
+//
+//        // Yêu cầu người dùng nhập vào một chuỗi
+//        System.out.println("Nhập vào một chuỗi: ");
+//        String input = scanner.nextLine();
+//
+//        // Đóng Scanner
+//        scanner.close();
+//
+//        // Tạo một HashMap để lưu trữ số lần xuất hiện của mỗi ký tự
+//        Map<Character, Integer> charCountMap = new HashMap<>();
+//
+//        // Đếm số lần xuất hiện của mỗi ký tự
+//        for (char ch : input.toCharArray()) {
+//            char lowerCaseChar = Character.toLowerCase(ch); // Đếm không phân biệt chữ hoa chữ thường
+//            charCountMap.put(lowerCaseChar, charCountMap.getOrDefault(lowerCaseChar, 0) + 1);
+//        }
+//
+//        // In ra kết quả
+//        System.out.println(input + " = 1");
+//        for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
+//            System.out.print(entry.getKey() + " = " + entry.getValue() +", ");
+//        }
+
+
+
     }
-    
+
 }
