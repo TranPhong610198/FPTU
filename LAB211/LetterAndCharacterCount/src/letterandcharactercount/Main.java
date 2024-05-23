74a19e31a015c51e05bee005148cde09b9dd676e
Moriarty <tphong610198@gmail.com>
2024-05-24 01:32:36 +0700

    Update letterAndCharacterCount into Lab211

--- LAB211/LetterAndCharacterCount/src/letterandcharactercount/Main.java
+++ LAB211/LetterAndCharacterCount/src/letterandcharactercount/Main.java
@@ -10,40 +10,37 @@ import java.util.*;
  *
  * @author tphon
  */
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
 
-//        // Tạo đối tượng Scanner để đọc đầu vào từ người dùng
-//        Scanner scanner = new Scanner(System.in);
-//
-//        // Yêu cầu người dùng nhập vào một chuỗi
-//        System.out.println("Nhập vào một chuỗi: ");
-//        String input = scanner.nextLine();
-//
-//        // Đóng Scanner
-//        scanner.close();
-//
-//        // Tạo một HashMap để lưu trữ số lần xuất hiện của mỗi ký tự
-//        Map<Character, Integer> charCountMap = new HashMap<>();
-//
-//        // Đếm số lần xuất hiện của mỗi ký tự
-//        for (char ch : input.toCharArray()) {
-//            char lowerCaseChar = Character.toLowerCase(ch); // Đếm không phân biệt chữ hoa chữ thường
-//            charCountMap.put(lowerCaseChar, charCountMap.getOrDefault(lowerCaseChar, 0) + 1);
-//        }
-//
-//        // In ra kết quả
-//        System.out.println(input + " = 1");
-//        for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
-//            System.out.print(entry.getKey() + " = " + entry.getValue() +", ");
-//        }
+        System.out.println("Input String: ");
+        String input = scanner.nextLine();
 
+        scanner.close();
+       
+        String[] words = input.trim().split("\\s+");
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String word : words) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
 
-
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (char ch : input.toLowerCase().toCharArray()) {
+            if (Character.isLetter(ch)) {
+                charCount.put(ch, charCount.getOrDefault(ch, 0) + 1);
+            }
+        }
+            
+        System.out.println(wordCount);
+      
+        System.out.println(charCount);
     }
-
 }
+
